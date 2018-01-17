4733079b7c042ccd5650c226a1a3af08b4ead23b
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-17 17:30:08 +0530

    Update CyclicRotation.py
    Solution updated.

--- 2-arrays/CyclicRotation.py
+++ 2-arrays/CyclicRotation.py
@@ -1,7 +1,9 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
-from functools import reduce
-def solution(A):
+from collections import deque
+def solution(A, K):
     # write your code in Python 3.6
-    return reduce(lambda x, y: x^y, A)
+    dq = deque(A)
+    dq.rotate(K)
+    
+    return list(dq)
