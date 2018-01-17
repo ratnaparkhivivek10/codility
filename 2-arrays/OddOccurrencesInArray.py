1afed5b3504368bea217b9a6969ec848110e4a07
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-17 17:29:32 +0530

    Update OddOccurrencesInArray.py
    Problem solved.

--- 2-arrays/OddOccurrencesInArray.py
+++ 2-arrays/OddOccurrencesInArray.py
@@ -1,6 +1,7 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+from functools import reduce
 def solution(A):
     # write your code in Python 3.6
-    pass
+    return reduce(lambda x, y: x^y, A)
