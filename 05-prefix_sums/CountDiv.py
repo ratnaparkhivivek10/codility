478dbea90c76f6bfe19767ddffc3d6abbcc00226
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-29 11:54:24 +0530

    CountDiv solution

--- 05-prefix_sums/CountDiv.py
+++ 05-prefix_sums/CountDiv.py
@@ -1,6 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(A):
+def solution(A, B, K):
     # write your code in Python 3.6
-    pass
+    return len(list(filter(lambda x: x%K == 0, range(A, B+1))))
