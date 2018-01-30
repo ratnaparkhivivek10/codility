6174f911fb85759630921fab5f40cb66ccdb32df
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-30 14:32:37 +0530

    Update GenomicRangeQuery.py

--- 05-prefix_sums/GenomicRangeQuery.py
+++ 05-prefix_sums/GenomicRangeQuery.py
@@ -4,5 +4,4 @@
 def solution(S, P, Q):
     # write your code in Python 3.6
     impact_factor = {'A':1, 'C':2, 'G':3, 'T':4}
-    
     return [impact_factor[k] for k in list(map(lambda p, q: min(S[p:q+1], key=lambda x: impact_factor.get(x)), P, Q))]
