1d1b9547e7d071eef643a698ebef8990b1bea28c
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-31 19:01:51 +0530

    Update FrogRiverOne.py

--- 04-counting_elements/FrogRiverOne.py
+++ 04-counting_elements/FrogRiverOne.py
@@ -3,4 +3,11 @@
 
 def solution(X, A):
     # write your code in Python 3.6
-    pass
+    posn = set()
+    for index, item in enumerate(A):
+        posn.add(item)
+        if len(posn) == X:
+            return index
+    else:
+        return -1
+            
