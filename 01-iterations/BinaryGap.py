40101168bcc0e35a8211470727bc08aa748625d0
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-01-31 13:29:48 +0530

    Second approach solution added

--- 01-iterations/BinaryGap.py
+++ 01-iterations/BinaryGap.py
@@ -5,3 +5,20 @@ def solution(N):
         return len(bg)
     else:
         return 0
+    
+# Second approach.
+def solution(N):
+    data = bin(N)[2:]
+    print(data)
+    count = 0
+    max = 0
+    for i in data:
+        if i == '0':
+            count+=1
+        else:
+            if max < count:
+                max = count
+                count = 0
+            else:
+                count = 0
+    return max
