33e905e5986063f073f85d6698ffd7cfc3a9b658
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-02-07 13:03:01 +0530

    Update solution.py

--- 08-leader/solution.py
+++ 08-leader/solution.py
@@ -3,4 +3,12 @@
 
 def solution(A):
     # write your code in Python 3.6
-    pass
+    leader = max(A, key=lambda x: A.count(x))
+    equi_leaders = 0
+    for i in range(1, len(A)):
+        seq1, seq2 = A[:i], A[i:]
+        
+        if seq1.count(leader) > len(seq1)/2 and seq2.count(leader) > len(seq2)/2:
+            equi_leaders += 1
+    
+    return equi_leaders
