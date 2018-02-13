d3dbb40a482b9bf58933d7df9a512b0dad4a6333
Vivek Ratnaparkhi <vivek.ratnaparkhi10@gmail.com>
2018-02-13 16:57:18 +0530

    Update and rename solution.py to CountFactors.py

--- 10-prime_and_composite_numbers/CountFactors.py
+++ 10-prime_and_composite_numbers/CountFactors.py
@@ -1,6 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
-def solution(A):
+# O(n) complexity
+def solution(N):
     # write your code in Python 3.6
-    pass
+    return len([n for n in range(1, N+1) if N%n == 0])
